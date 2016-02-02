3abf6df9d1e3c8bcc185b4554a00e3c828a3f599
Henry Read <henrya2@126.com>
2016-02-02 14:44:17 +0800

    Create.......

--- OpenGEXTest/Plugins/OpenGEXImporter/Source/OpenGEXImporter/Classes/OpenGEXImportFactory.h
+++ OpenGEXTest/Plugins/OpenGEXImporter/Source/OpenGEXImporter/Classes/OpenGEXImportFactory.h
@@ -14,11 +14,17 @@ class UOpenGEXImportFactory : public UFactory
 public:
 	UOpenGEXImportFactory();
 
+	// Begin UObject Interface
+	virtual void CleanUp() override;
+	virtual bool ConfigureProperties() override;
+	virtual void PostInitProperties() override;
+	// End UObject Interface
+
 	// Begin UFactory Interface
 	virtual FText GetDisplayName() const override;
 	virtual bool DoesSupportClass(UClass * Class) override;
 	virtual UClass* ResolveSupportedClass() override;
-	virtual UObject* FactoryCreateBinary(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn) override;
+	virtual UObject* FactoryCreateBinary(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled) override;
 	virtual bool FactoryCanImport(const FString& Filename) override;
 	// End UFactory Interface
 };
--- OpenGEXTest/Plugins/OpenGEXImporter/Source/OpenGEXImporter/Private/OpenGEXImportFactory.cpp
+++ OpenGEXTest/Plugins/OpenGEXImporter/Source/OpenGEXImporter/Private/OpenGEXImportFactory.cpp
@@ -8,8 +8,27 @@
 UOpenGEXImportFactory::UOpenGEXImportFactory()
 	: Super(FObjectInitializer::Get())
 {
+	bText = false;
 	bEditorImport = true;
 	Formats.Add(TEXT("ogex;OpenGEX meshes and animations"));
+}
+
+void UOpenGEXImportFactory::CleanUp()
+{
+
+}
+
+bool UOpenGEXImportFactory::ConfigureProperties()
+{
+	return true;
+}
+
+void UOpenGEXImportFactory::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	bText = false;
+	bEditorImport = true;
 }
 
 FText UOpenGEXImportFactory::GetDisplayName() const
@@ -27,7 +46,7 @@ UClass* UOpenGEXImportFactory::ResolveSupportedClass()
 	return UStaticMesh::StaticClass();
 }
 
-UObject* UOpenGEXImportFactory::FactoryCreateBinary(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn)
+UObject* UOpenGEXImportFactory::FactoryCreateBinary(UClass* InClass, UObject* InParent, FName InName, EObjectFlags Flags, UObject* Context, const TCHAR* Type, const uint8*& Buffer, const uint8* BufferEnd, FFeedbackContext* Warn, bool& bOutOperationCanceled)
 {
 	return nullptr;
 }
